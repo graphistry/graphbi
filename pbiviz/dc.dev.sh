19cc2d98729076b30818ca6ad2a08eaca5a533b5
Leo Meyerovich <leo@graphistry.com>
2021-11-16 19:17:38 -0800

    infra(docker)

---
+++ pbiviz/dc.dev.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -ex
+
+#############
+#
+# Helper for running docker compose commands in dev
+# ex: follow logs -    ./dc.dev.sh logs -f
+#
+#############
+
+export CERTS_PATH="$(npm list -g --silent | head -1)/node_modules/powerbi-visuals-tools/certs"
+echo "Running with host CERTS_PATH: ${CERTS_PATH}"
+if [ ! -d "${CERTS_PATH}" ]; then
+  echo "ERROR: Certs path not found: ${CERTS_PATH}"
+  exit 1
+fi
+
+CERTS_PATH="${CERTS_PATH}" \
+PYTHONPATH="${PYTHONPATH}" \
+APP_BUILD_TAG=`cat VERSION`-dev \
+VERSION=`cat VERSION` \
+docker compose \
+  -f docker-compose.dev.yml \
+  $@
+
+
