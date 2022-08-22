1c967293c5ce5b0967c37cfa677288cad52b988b
Desirree Adegunle <87389186+dess890@users.noreply.github.com>
2022-08-22 11:13:15 -0400

    fix(Loader.ts): added sso loop to loader

--- pbiviz/src/components/Loader.tsx
+++ pbiviz/src/components/Loader.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
 import { LoadState } from '../LoadState';
 
+
+
+
 function isConfigureAccountReady(config) {
     return config.UrlBase !== '' && config.UserName !== '' && config.Password !== '';
 }
@@ -13,21 +16,62 @@ function isBindingsReady(view) {
     );
 }
 
-// tslint:disable-next-line
-function Loader({ view, config, state }) {
-    console.debug('Loader::render', { view, config, state });
 
+const getSSO = async (client, host) => {
+    console.log('getSSO');
+    const authResponse = await client.getAuthUrl();
+    console.log('checking ssoLogin');
+    host.launchUrl(authResponse.auth_url);
+    // await client.delay (5000);
+    const startedMS = Date.now();
+    await client.delay(200);
+    console.log('getSSO'); //short initial delay as may already be logged in
+    while(true) {
+        console.log('ssologin loop');
+        await client.delay(20000);
+        const response = await (client.fetch)(`https://eks-skinny.grph.xyz/api/v2/o/sso/oidc/jwt/${authResponse.state}/`, {
+            method: 'GET', 
+        });
+        await client.delay(15000);
+        console.log('authenticated');
+        console.info('ssoLogin response');
+        const data = await response.json(); // only do this if theyre done being authenticated/logged in
+        if (data) { //TODO and whatever success conditions
+            console.log('data with good jwt', data.data.token);
+            const newIframe = `/accounts/login/jwt/?token=${data.data.token}&next=graph/graph.html`;
+            console.log('this is the newiframe', newIframe);
+            return newIframe;
+        }
+        //TODO check if expected not-ready rejection, else throw unexpected error
+        if (Date.now() - startedMS > 60000) {
+            console.error('sso login timed out');
+            throw new Error('sso login timed out');
+        }
+        await client.delay(1000);
+}
+}
+// const preJwt = async (client): Promise<any> => {
+//     const afterClick = await clickedAuthUrl(client);
+//     setTimeout()
+//     client.authenticated = true
+//     return afterClick;
+// }
+
+
+// tslint:disable-next-line
+function Loader({ view, config, state, host, client }) {
+    console.debug('Loader::render', { view, config, state, host });
+  
     if (state !== LoadState.MISCONFIGURED) {
         return null;
     }
-
     return (
         <>
             <h1>Graphistry for PowerBI Custom </h1>
             <h2>
                 1. Configure account in <code>Format panel: Graphistry Settings</code>
             </h2>
-            {isConfigureAccountReady(config) ? (
+            {isConfigureAccountReady(config)? ( //fix
                 <span>✔️ Done</span>
             ) : (
                 <ul>
@@ -48,7 +92,7 @@ function Loader({ view, config, state }) {
                     ) : null}
                 </ul>
             )}
-            <h2>
+             <h2>
                 2. Bind edge source/destination in <code>Fields panel</code>
             </h2>
             {isBindingsReady(view) ? (
@@ -66,9 +110,17 @@ function Loader({ view, config, state }) {
                         </li>
                     ) : null}
                 </ul>
-            )}
+
+            )} 
+             <h2>
+                 3. Sign in with SSO
+            </h2>
+                <ul>
+                    <li>
+                        <button onClick={ async () => { await getSSO(client,host)} }>Sign in</button>
+                    </li>
+                </ul>
         </>
     );
 }
-
 export { Loader };
