0d0df0879a292d7da4344285a6dc402eba68b567
Desirree Adegunle <87389186+dess890@users.noreply.github.com>
2022-08-22 11:13:49 -0400

    fix(Main.tsx): added client for Main to take in

--- pbiviz/src/components/Main.tsx
+++ pbiviz/src/components/Main.tsx
@@ -3,10 +3,10 @@ import { Loader } from './Loader';
 import { Graphistry } from './Graphistry';
 
 // tslint:disable-next-line
-function Main({v, host, view, config, datasetID, state, numNodes, numEdges}) {
-    console.debug('Main::render', { v, view, config, state });
+function Main({v, host, view, config, datasetID, state, numNodes, numEdges, client}) {
+    console.debug('Main::render', { v, view, config, state, host });
     return (<>
-        <Loader view={view} config={config} state={state}/>
+        <Loader view={view} config={config} state={state} host={host} client={client}/>
         <Graphistry
             host={host} view={view} config={config} state={state}
             datasetID={datasetID} numNodes={numNodes} numEdges={numEdges}/>
@@ -14,3 +14,4 @@ function Main({v, host, view, config, datasetID, state, numNodes, numEdges}) {
 }
 
 export { Main };
+
