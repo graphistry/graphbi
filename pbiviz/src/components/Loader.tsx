821d203e476a4127b7e0ad7c1b93658c790068c2
Desirree Adegunle <87389186+dess890@users.noreply.github.com>
2022-10-13 13:22:35 -0400

    fix(Loader.tsx): moved and simplified ssoLogin

--- pbiviz/src/components/Loader.tsx
+++ pbiviz/src/components/Loader.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { LoadState } from '../LoadState';
+import { config } from '../services/GraphistryClient';
 
 
 
@@ -21,46 +22,14 @@ const getSSO = async (client, host) => {
     console.log('getSSO');
     const authResponse = await client.getAuthUrl();
     console.log('checking ssoLogin');
-    host.launchUrl(authResponse.auth_url);
-    // await client.delay (5000);
-    const startedMS = Date.now();
-    await client.delay(200);
-    console.log('getSSO'); //short initial delay as may already be logged in
-    while(true) {
-        console.log('ssologin loop');
-        await client.delay(20000);
-        const response = await (client.fetch)(`https://eks-skinny.grph.xyz/api/v2/o/sso/oidc/jwt/${authResponse.state}/`, {
-            method: 'GET', 
-        });
-        await client.delay(15000);
-        console.log('authenticated');
-        console.info('ssoLogin response');
-        const data = await response.json(); // only do this if theyre done being authenticated/logged in
-        if (data) { //TODO and whatever success conditions
-            console.log('data with good jwt', data.data.token);
-            const newIframe = `/accounts/login/jwt/?token=${data.data.token}&next=graph/graph.html`;
-            console.log('this is the newiframe', newIframe);
-            return newIframe;
-        }
-        //TODO check if expected not-ready rejection, else throw unexpected error
-        if (Date.now() - startedMS > 60000) {
-            console.error('sso login timed out');
-            throw new Error('sso login timed out');
-        }
-        await client.delay(1000);
+    (host.launchUrl(authResponse.auth_url))
+    client.ssoLogin(authResponse);
 }
-}
-// const preJwt = async (client): Promise<any> => {
-//     const afterClick = await clickedAuthUrl(client);
-//     setTimeout()
-//     client.authenticated = true
-//     return afterClick;
-// }
-
 
 // tslint:disable-next-line
 function Loader({ view, config, state, host, client }) {
     console.debug('Loader::render', { view, config, state, host });
+    console.log('this is the client again', client.isSSO);
   
     if (state !== LoadState.MISCONFIGURED) {
         return null;
@@ -71,7 +40,7 @@ function Loader({ view, config, state, host, client }) {
             <h2>
                 1. Configure account in <code>Format panel: Graphistry Settings</code>
             </h2>
-            {isConfigureAccountReady(config)? ( //fix
+            {isConfigureAccountReady(config) || client.isSSO ? ( //fix
                 <span>✔️ Done</span>
             ) : (
                 <ul>
